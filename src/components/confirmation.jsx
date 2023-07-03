f574da1c799224e94d8d3b8eeb8d0c876409c318
daom bae <da6970@gmail.com>
2023-07-03 11:28:00 +0900

    event

---
+++ src/components/confirmation.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// function ConfirmationPage() {
+//   // Add your code for confirmation page here
+//   return (
+//     <div>
+//       <h2>Confirmation</h2>
+//       {/* Add your content and components */}
+//     </div>
+//   );
+// }
+
+// export default ConfirmationPage;
